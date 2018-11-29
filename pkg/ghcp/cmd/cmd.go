8801254152444ca07ea9dd9704a155314d21050d
izumin5210 <m@izum.in>
2018-11-29 10:48:43 +0900

    Fix impl

--- pkg/ghcp/cmd/cmd.go
+++ pkg/ghcp/cmd/cmd.go
@@ -23,76 +23,30 @@ func NewGhcpCommand(c *ghcp.Ctx) *cobra.Command {
 			ctx := context.Background()
 			gc := githubClient(ctx)
 
-			// get master's ref
-			masterRef, _, err := gc.Git.GetRef(ctx, owner, repo, "heads/master")
-			if err != nil {
-				return err
-			}
-
-			// create branch
-			featureRef, _, err := gc.Git.CreateRef(ctx, owner, repo, &github.Reference{
-				Ref:    "heads/" + branch,
-				Object: masterRef.Object,
-			})
-			if err != nil {
-				return err
-			}
-
-			// get HEAD commit
-			headCommit, _, err := gc.Git.GetCommit(ctx, owner, repo, featureRef.Object.SHA)
-			if err != nil {
-				return err
-			}
-
-			// create blob
-			blob, _, err := gc.Git.CreateBlob(ctx, owner, repo, &github.Blob{
-				Content:  "foobarbaz",
-				Encoding: "utf-8",
-			})
-			if err != nil {
-				return err
-			}
-
-			tree, _, err := gc.Git.CreateTree(ctx, owner, repo, headCommit.Tree.SHA, []github.TreeEntry{
-				{
-					Path: "test.txt",
-					Mode: "100644",
-					Type: "blob",
-					SHA:  blob.SHA,
-				},
-			})
-			if err != nil {
-				return err
-			}
-
-			commit, _, err := gc.Git.CreateCommit(ctx, owner, repo, &github.Commit{
-				Message: "test commit",
-				Author: &github.CommitAuthor{
+			svc := &service{
+				Client: gc,
+				Author: Author{
 					Name:  "izumin5210",
 					Email: "m@izum.in",
-					Date:  time.Now().Format("2006-01-02T15:04:05-0700"),
 				},
-				Parents: []github.Commit{headCommit},
-				Tree:    tree.SHA,
-			})
+			}
+
+			ref, err := svc.FindOrCreateBranch(ctx, owner, repo, "master", branch)
 			if err != nil {
 				return err
 			}
 
-			_, _, err = gc.Git.UpdateRef(ctx, owner, repo, &github.Reference{
-				Ref: branch,
-				SHA: commit.SHA,
-			}, false)
+			err = svc.CreateOrUpdateCommit(ctx, owner, repo, ref, []File{
+				{
+					Path:    "test.txt",
+					Content: "foobarbaz",
+				},
+			}, "test commit")
 			if err != nil {
 				return err
 			}
 
-			pr, _, err := gc.PullRequests.Create(ctx, owner, repo, &github.NewPullRequest{
-				Title: "test pull request",
-				Head:  branch,
-				Base:  "master",
-				Body:  "This is a test pull request.",
-			})
+			pr, err := svc.CreateOrUpdatePullRequest(ctx, owner, repo, "master", branch, "test pull request", "This is test pull request")
 			if err != nil {
 				return err
 			}
@@ -117,4 +71,119 @@ func httpClient(ctx context.Context) *http.Client {
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
 	tc := oauth2.NewClient(ctx, ts)
 	return tc
+}
+
+type service struct {
+	*github.Client
+
+	Author Author
+}
+
+type Author struct {
+	Name  string
+	Email string
+}
+
+type File struct {
+	Path    string
+	Content string
+}
+
+func (s *service) FindOrCreateBranch(ctx context.Context, owner, repo string, baseBranch, featureBranch string) (
+	*github.Reference,
+	error,
+) {
+	baseBranch = "heads/" + baseBranch
+	featureBranch = "heads/" + featureBranch
+
+	// TODO: find an existing branch
+
+	// get master's ref
+	masterRef, _, err := s.Git.GetRef(ctx, owner, repo, baseBranch)
+	if err != nil {
+		return nil, err
+	}
+
+	// create branch
+	featureRef, _, err := s.Git.CreateRef(ctx, owner, repo, &github.Reference{
+		Ref:    &featureBranch,
+		Object: masterRef.Object,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return featureRef, nil
+}
+
+func (s *service) CreateOrUpdateCommit(ctx context.Context, owner, repo string, ref *github.Reference, files []File, msg string) error {
+	headCommit, _, err := s.Git.GetCommit(ctx, owner, repo, *ref.Object.SHA)
+	if err != nil {
+		return err
+	}
+
+	var (
+		encoding  = "utf-8"
+		entryMode = "100644"
+		entryType = "blob"
+		entries   []github.TreeEntry
+	)
+	for _, f := range files {
+		blob, _, err := s.Git.CreateBlob(ctx, owner, repo, &github.Blob{
+			Content:  &f.Content,
+			Encoding: &encoding,
+		})
+		if err != nil {
+			return err
+		}
+
+		entries = append(entries, github.TreeEntry{
+			Path: &f.Path,
+			Mode: &entryMode,
+			Type: &entryType,
+			SHA:  blob.SHA,
+		})
+	}
+
+	tree, _, err := s.Git.CreateTree(ctx, owner, repo, *headCommit.Tree.SHA, entries)
+	if err != nil {
+		return err
+	}
+
+	date := time.Now()
+	commit, _, err := s.Git.CreateCommit(ctx, owner, repo, &github.Commit{
+		Message: &msg,
+		Author: &github.CommitAuthor{
+			Name:  &s.Author.Name,
+			Email: &s.Author.Email,
+			Date:  &date,
+		},
+		Parents: []github.Commit{*headCommit},
+		Tree:    tree,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, _, err = s.Git.UpdateRef(ctx, owner, repo, &github.Reference{
+		Ref: ref.Ref,
+		Object: &github.GitObject{
+			SHA: commit.SHA,
+		},
+	}, false)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *service) CreateOrUpdatePullRequest(ctx context.Context, owner, repo string, baseBranch, featureBranch, title, body string) (*github.PullRequest, error) {
+	pr, _, err := s.PullRequests.Create(ctx, owner, repo, &github.NewPullRequest{
+		Title: &title,
+		Head:  &featureBranch,
+		Base:  &baseBranch,
+		Body:  &body,
+	})
+	return pr, err
 }
